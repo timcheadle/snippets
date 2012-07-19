fcf08259146680faa2c00c530bb850dd023ce3bb
Tim Cheadle <tim@rationalmeans.com>
2012-07-18 21:15:13 -0400

    Added full_title helper, removed home title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,7 +9,12 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "| Home")
+      page.should have_selector('title', :text => "Snippets")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
