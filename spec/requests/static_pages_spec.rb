3ee8bfc76018c765b751488da90b061d4f33fe3e
Tim Cheadle <tim@rationalmeans.com>
2012-08-23 17:14:33 -0400

    Added Users controller and tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,28 +7,28 @@ describe "Static pages" do
     before { visit root_path }
 
     it { should have_content('Snippets') }
-    it { should have_selector('title', :text => 'Snippets') }
-    it { should_not have_selector('title', :text => '| Home') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector('title', text: '| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
 
     it { should have_content('Help') }
-    it { should have_selector('title', :text => "| Help") }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
     it { should have_content('About Us') }
-    it { should have_selector('title', :text => "| About Us") }
+    it { should have_selector('title', text: full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_selector('h1', :text => 'Contact') }
-    it { should have_selector('title', :text => '| Contact') }
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
 end
