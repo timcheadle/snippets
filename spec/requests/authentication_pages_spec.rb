76302438169a3da795956dc9e693f8f0e467dc42
Tim Cheadle <tim@rationalmeans.com>
2012-08-30 11:26:04 -0400

    Added following/followers interaction

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -92,6 +92,30 @@ describe "AuthenticationPages" do
           before { visit users_path }
           it { should have_selector('title', text: "Sign in") }
         end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: "Sign in") }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_selector('title', text: "Sign in") }
+        end
+      end
+    end
+
+    describe "in the Relationships controller" do
+      describe "submitting to the create action" do
+        before { post relationships_path }
+        specify { response.should redirect_to(signin_path) }
+      end
+
+      describe "submitting to the destroy action" do
+        # Hard-coded ID of 1 because the non-signed-in user should never
+        # actually be able to execute this, so ID shouldn't matter
+        before { delete relationship_path(1) }
+        specify { response.should redirect_to(signin_path) }          
       end
     end
 
